6b8c0139fb083875bd1a795f858f13495100ad06
helphin <helphin.daniston@gmail.com>
2023-07-13 12:42:52 +0400

    config constant added

---
+++ src/app/constants/app.constant.ts
@@ -0,0 +1,3 @@
+export const AppConstant: any = {
+    POPULAR_CURRENCIES: 'GBP, JPY, USD, AUD, CAD, CHF, CNH, HKD, NZD, CNY, SGD'
+}
---
+++ src/app/constants/currency-list.constant.ts
@@ -0,0 +1,53 @@
+import { CurrencyOption } from "../models/currency-option";
+
+export const CurrencyListConstant: CurrencyOption[] = [
+    {
+        currencyName: 'EURO',
+        currencyCode: 'EUR',
+    },
+    {
+        currencyName: 'GBP',
+        currencyCode: 'GBP',
+    },
+    {
+        currencyName: 'JPY',
+        currencyCode: 'JPY',
+    },
+    {
+        currencyName: 'USD',
+        currencyCode: 'USD',
+    },
+    {
+        currencyName: 'AUD',
+        currencyCode: 'AUD',
+    },
+    {
+        currencyName: 'CAD',
+        currencyCode: 'CAD',
+    },
+    {
+        currencyName: 'CHF',
+        currencyCode: 'CHF',
+    },
+    {
+        currencyName: 'CNH',
+        currencyCode: 'CNH',
+    },
+    {
+        currencyName: 'HKD',
+        currencyCode: 'HKD',
+    },
+    {
+        currencyName: 'NZD',
+        currencyCode: 'NZD',
+    },
+    {
+        currencyName: 'CNY',
+        currencyCode: 'CNY',
+    },
+    {
+        currencyName: 'SGD',
+        currencyCode: 'SGD',
+    },
+]
+
